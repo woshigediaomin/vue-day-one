e67207a86ee006bb0498038d2d00d48b1f4ad0fc
woshigediaomin <2454074446@qq.com>
2022-06-17 22:43:50 +0800

    打包css文件

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+    mode: "development",
     entry: './src/main.js',//入口
     output: {
         path: path.resolve(__dirname, 'dist'),//出口
@@ -9,5 +10,19 @@ module.exports = {
     },
     plugins: [new HtmlWebpackPlugin({
         template:'./public/index.html'
-    })]
+    })],
+    devServer: {
+        //yarn serve 自动打开浏览器
+        open: true,
+        //0-65535
+        port: 52525
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use:["style-loader","css-loader"]
+            }
+        ]
+    }
 }
